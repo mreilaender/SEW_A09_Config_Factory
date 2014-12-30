84cb9ddccacc4a085a895f2fe4f0ce90b7230dfb
mreilaender <mreilaender@student.tgm.ac.at>
2014-12-30 20:22:21 +0100

    Added comments

--- src/start/Connector.java
+++ src/start/Connector.java
@@ -13,6 +13,10 @@ public class Connector {
 	private MysqlDataSource ds;
 	private Parser parser;
 	
+	/**
+	 * 
+	 * @param parser Needed to set Username, password and something like that.
+	 */
 	public Connector(Parser parser) {
 		this.parser = parser;
 	}
@@ -26,7 +30,6 @@ public class Connector {
 				parser.PASSWORD = "manuel";
 			}
 		}
-		
 		ds.setServerName(parser.IP);
 		ds.setUser(parser.USERNAME);
 		ds.setPassword(parser.PASSWORD);
@@ -35,7 +38,28 @@ public class Connector {
 		con = ds.getConnection();
 		st = con.createStatement();
 	}
+	/**
+	 * Executes a query
+	 * @param query Query to be executed
+	 * @return ResultSet of the query
+	 * @throws SQLException Exception will be thrown if the connection wasn't successfully created.
+	 */
 	public ResultSet executeQuery(String query) throws SQLException {
 		return st.executeQuery(query);
+	}
+	/**
+	 * Closes all Streams (Statement, Connection)
+	 */
+	public void closeStreams() {
+		try {
+			st.close();
+		} catch (SQLException e) {
+			System.err.println("Statement has already been closed");
+		}
+		try {
+			con.close();
+		} catch (SQLException e) {
+			System.err.println("Connection has already been closed");
+		}
 	}
 }
