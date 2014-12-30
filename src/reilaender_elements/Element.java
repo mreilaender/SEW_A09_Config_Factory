b8e19b5fd3231ebc3dd573455c4db709ca49a7b9
mreilaender <mreilaender@student.tgm.ac.at>
2014-12-30 20:24:05 +0100

    Added comments

--- src/reilaender_elements/Element.java
+++ src/reilaender_elements/Element.java
@@ -1,5 +1,9 @@
 package reilaender_elements;
 
 public interface Element {
+	/**
+	 * 
+	 * @return Formated String made after the specific file syntax (INI, PHP, XML)
+	 */
 	public String getFormat();
 }
